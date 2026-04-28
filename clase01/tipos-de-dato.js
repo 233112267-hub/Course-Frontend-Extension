6ddbb56ed9d4b229378c569c51847704511beafa
233112267-hub <233112267@upmh.edu.mx>
2026-04-27 20:01:09 -0600

    Cuarto practicaterminada-clase01

---
+++ clase01/tipos-de-dato.js
@@ -0,0 +1,16 @@
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof '');
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+//Ejemplos propios
+console.log(typeof 3.14);
+console.log(typeof 'Big Poppa');
+console.log(typeof 'saludame');   
+console.log(typeof '0.19');
+console.log(typeof -0.028);   
+console.log(typeof 5e-5);   
