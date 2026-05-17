dd1f3fca0662d1cede7933f777c128a09b4765f2
233112267-hub <233112267@upmh.edu.mx>
2026-05-17 08:32:42 -0600

    Septimo commit-Proyecto 4 Libros Leidos

---
+++ Proyecto4/script.js
@@ -0,0 +1,30 @@
+/*Problema: Seguimiento de Libros
+Crea un sistema muy sencillo para hacer seguimiento de los libros que has leído.
+
+Instrucciones para resolver el problema:
+Define una función `agregarLibro(titulo)`, que añada un libro a un array llamado `librosLeidos`.
+Define una función `mostrarLibrosLeidos()`, que imprima todos los libros que has leído.*/
+ 
+titulo= "¿Escribe los kibros que has leido ultimamente?";
+
+librosLeidos = [];
+
+function agregarLibro(titulo) {
+    librosLeidos.push(titulo);
+}
+
+function mostrarLibrosLeidos() {
+    console.log("Libros que has leído:");
+    for (let i=0; i < librosLeidos.length; i++) {
+        console.log(librosLeidos[i]);
+    }
+    /* Personalmente uso mas el of  ya que que sirve tambien para objetos poder  recorrerlos 
+    for ( let leido of librosLeidos) {
+        console.log(leido);
+    }   */
+}
+
+agregarLibro("Las Aventuras de Sherlock Holmes");
+agregarLibro("Cien Años de Soledad");
+agregarLibro("Las Vacas Quiviquitas");
+mostrarLibrosLeidos();
